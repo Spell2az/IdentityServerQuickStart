38d76ef22817a35375841872dd2d8e3d91bd302e
Martin Kovac <martin.kovac@myunidays.com>
2020-01-22 16:27:02 +0000

    Initial

---
+++ src/Api/IdentityController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api
+{
+    [Microsoft.AspNetCore.Components.Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new
+            {
+                c.Type, c.Value
+            });
+        }
+    }
+}
